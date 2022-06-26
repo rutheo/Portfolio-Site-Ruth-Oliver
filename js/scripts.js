6a7e9819cfc8f020148a2e3ce27aa8299716bf7a
rutheo <107320328+rutheo@users.noreply.github.com>
2022-06-26 14:44:05 +0100

    User Testing & Accessibility
    Updates completed as a result of user testing feedback. Changes made to improve accessibility and code quality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,17 @@
 console.log("Hi, welcome to Ruth Oliver's protfolio website!")
 
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'navtoggle';
-    }
-    var icon=document.getElementsByClassName("fa-bars")[0];
-    if(icon){
-       icon.className = icon.className.replace("fa-bars", "fa-xmark");
-    }
-    else{
-    icon=document.getElementsByClassName("fa-xmark")[0];
-     icon.className = icon.className.replace("fa-xmark", "fa-bars");
-    }
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+  var icon = document.getElementsByClassName("fa-bars")[0];
+  if (icon) {
+    icon.className = icon.className.replace("fa-bars", "fa-xmark");
+  } else {
+    icon = document.getElementsByClassName("fa-xmark")[0];
+    icon.className = icon.className.replace("fa-xmark", "fa-bars");
+  }
 }
